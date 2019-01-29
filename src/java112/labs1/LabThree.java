a35f250432b975d6f51c35d0db33034f40f54ac8
Tom Good <good.madisoncollege.edu>
2019-01-29 17:16:28 -0600

    Added class LabThree.java

--- src/java112/labs1/LabThree.java
+++ src/java112/labs1/LabThree.java
@@ -2,8 +2,15 @@ package java112.labs1;
 
 public class LabThree
 {
-        public int getDeclaredFields0()
-        {
+    String test = "test";
 
-        }
+    public void run(String input)
+    {
+        System.out.println("input: " + test);
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
 }
