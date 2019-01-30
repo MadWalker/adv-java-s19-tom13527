b0abe6b4a208ffba177cadd2735e55360be3ff15
Tom Good <good.madisoncollege.edu>
2019-01-30 12:06:27 -0600

    added boolean to lab 3

--- src/java112/labs1/LabThree.java
+++ src/java112/labs1/LabThree.java
@@ -6,11 +6,20 @@ public class LabThree
 
     public void run(String input)
     {
-        System.out.println("input: " + input);
+            System.out.println("input: " + input);
     }
 
     public static void main(String[] args)
     {
+        LabThree output = new LabThree();
 
+        if (args.length != 1)
+        {
+            System.out.println("Please enter one argument on the command line");
+        }
+        else
+        {
+            output.run("some string");
+        }
     }
 }
