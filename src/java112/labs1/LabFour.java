29a754a6d713f7f39bd715a377dc9895aa33b2a2
Tom Good <good.madisoncollege.edu>
2019-01-31 13:17:26 -0600

    added bufferedreader to lab 4

--- src/java112/labs1/LabFour.java
+++ src/java112/labs1/LabFour.java
@@ -1,7 +1,55 @@
 package java112.labs1;
+import java.util.*;
 
 public class LabFour
 {
+
+    public void run(String input)
+    {
+        BufferedReader inputReader = null;
+
+        try
+        {
+            inputReader = new BufferedReader(new FileReader(args));
+
+            // Code to process the input file
+            // Declare a String variable to hold the current line
+
+            String line = null;
+
+            while (inputReader.ready())
+            {
+                // Read each line in here
+                line = inputReader.readLine();
+
+                // Now Process the line in some way
+                // This will usually be in another method that we
+                // pass the line to
+            }
+    }
+    catch (FileNotFoundException fileNotFound)
+    {
+        fileNotFound.printStackTrace();
+    }
+    catch (IOException ioExcetion)
+    {
+        ioException.printStackTrace();
+    }
+    finally
+    {
+        try
+        {
+            if (inputReader != null)
+            {
+                inputReader.close();
+            }
+        }
+        catch (Exception exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+
     public static void main(String[] args)
     {
         LabFour labFour = new LabFour();
@@ -13,7 +61,8 @@ public class LabFour
         }
         else
         {
-            labFour.run();
+            labFour.run(args[0]);
         }
     }
+
 }
